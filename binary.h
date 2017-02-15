3bfd10dbb93f237226ea59bf17ab241d6085cf87
Armen Berujanyan <armenberug@gmail.com>
2017-02-15 19:35:49 +0400

    Minor Refactorings

--- binary.h
+++ binary.h
@@ -1,18 +1,13 @@
-// ----------------------------------------------------- //
-//                                                       //
-//              Binary Writer/Reader                     //
-//               Copyright (C) 2016                      //
-//                  by NucleArc                          //
-//                                                       //
-// ----------------------------------------------------- //
+/* 
+    Binary Reader/Writer
+    Copyright (C) 2016-2017 by NuclearC
+*/
 
 #pragma once
 
-#ifndef BINARY_H_
-#define BINARY_H_
+#ifndef NC_BINARY_H_
+#define NC_BINARY_H_
 #include <vector>
-#include <cstring>
-#include <cstdint>
 
 template<typename _T>
 static void writeToVector(std::vector<char>& _Buffer, unsigned long long& _Offset, const _T& _Val, bool _Le = true)
@@ -67,36 +62,86 @@ static _T readFromBuffer(const char* _Buffer, const unsigned long long& _Size, u
 	return _Result;
 }
 
+template<typename _T>
+static _T readFromBuffer(const char* _Buffer, const unsigned long long& _Size, const unsigned long long& _Offset, bool _Le = true)
+{
+    _T _Result = 0;
+    unsigned long long size = sizeof(_T);
+    if (size + _Offset > _Size)
+        return _Result;
+
+    memcpy(&_Result, _Buffer + _Offset, size);
+    return _Result;
+}
+
+template<typename _T>
+static _T readFromBuffer(const unsigned char* _Buffer, const unsigned long long& _Size, unsigned long long& _Offset, bool _Le = true)
+{
+    return readFromBuffer((const char*)_Buffer, _Size, _Offset, _Le);
+}
+
 static int readInt32(const char * _Buffer, const unsigned long long& _Size, unsigned long long& _Offset, bool _Le = true) {
 	return readFromBuffer<int>(_Buffer, _Size, _Offset, _Le);
+}
+
+static int readInt32(const char * _Buffer, const unsigned long long& _Size, const unsigned long long& _Offset, bool _Le = true) {
+    return readFromBuffer<int>(_Buffer, _Size, _Offset, _Le);
 }
 
 static short int readInt16(const char * _Buffer, const unsigned long long& _Size, unsigned long long& _Offset, bool _Le = true) {
 	return readFromBuffer<short>(_Buffer, _Size, _Offset, _Le);
 }
 
+static short int readInt16(const char * _Buffer, const unsigned long long& _Size, const unsigned long long& _Offset, bool _Le = true) {
+    return readFromBuffer<short>(_Buffer, _Size, _Offset, _Le);
+}
+
 static char readInt8(const char * _Buffer, const unsigned long long& _Size, unsigned long long& _Offset, bool _Le = true) {
 	return readFromBuffer<char>(_Buffer, _Size, _Offset, _Le);
+}
+
+static char readInt8(const char * _Buffer, const unsigned long long& _Size, const unsigned long long& _Offset, bool _Le = true) {
+    return readFromBuffer<char>(_Buffer, _Size, _Offset, _Le);
 }
 
 static unsigned int readUint32(const char * _Buffer, const unsigned long long& _Size, unsigned long long& _Offset, bool _Le = true) {
 	return readFromBuffer<unsigned int>(_Buffer, _Size, _Offset, _Le);
 }
 
+static unsigned int readUint32(const char * _Buffer, const unsigned long long& _Size, const unsigned long long& _Offset, bool _Le = true) {
+    return readFromBuffer<unsigned int>(_Buffer, _Size, _Offset, _Le);
+}
+
 static unsigned short int readUint16(const char * _Buffer, const unsigned long long& _Size, unsigned long long& _Offset, bool _Le = true) {
 	return readFromBuffer<unsigned short>(_Buffer, _Size, _Offset, _Le);
+}
+
+static unsigned short int readUint16(const char * _Buffer, const unsigned long long& _Size, const unsigned long long& _Offset, bool _Le = true) {
+    return readFromBuffer<unsigned short>(_Buffer, _Size, _Offset, _Le);
 }
 
 static unsigned char readUint8(const char * _Buffer, const unsigned long long& _Size, unsigned long long& _Offset, bool _Le = true) {
 	return readFromBuffer<unsigned char>(_Buffer, _Size, _Offset, _Le);
 }
 
+static unsigned char readUint8(const char * _Buffer, const unsigned long long& _Size, const unsigned long long& _Offset, bool _Le = true) {
+    return readFromBuffer<unsigned char>(_Buffer, _Size, _Offset, _Le);
+}
+
 static float readFloat(const char * _Buffer, const unsigned long long& _Size, unsigned long long& _Offset, bool _Le = true) {
 	return readFromBuffer<float>(_Buffer, _Size, _Offset, _Le);
 }
 
+static float readFloat(const char * _Buffer, const unsigned long long& _Size, const unsigned long long& _Offset, bool _Le = true) {
+    return readFromBuffer<float>(_Buffer, _Size, _Offset, _Le);
+}
+
 static double readDouble(const char * _Buffer, const unsigned long long& _Size, unsigned long long& _Offset, bool _Le = true) {
 	return readFromBuffer<double>(_Buffer, _Size, _Offset, _Le);
+}
+
+static double readDouble(const char * _Buffer, const unsigned long long& _Size, const unsigned long long& _Offset, bool _Le = true) {
+    return readFromBuffer<double>(_Buffer, _Size, _Offset, _Le);
 }
 
 static void writeInt32(char* _Buffer, unsigned long long& _Offset, const int& _Val, bool _Le = true) {
@@ -130,4 +175,5 @@ static void writeFloat(char* _Buffer, unsigned long long& _Offset, const float& 
 static void writeDouble(char* _Buffer, unsigned long long& _Offset, const double& _Val, bool _Le = true) {
 	return writeToBuffer<double>(_Buffer, _Offset, _Val, _Le);
 }
-#endif // BINARY_H_
+
+#endif // NC_BINARY_H_
