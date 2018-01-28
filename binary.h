d3adc179cf5fc1ad72bf7ec55504cf2c79ace096
NuclearC <NuclearC@users.noreply.github.com>
2018-01-28 17:32:19 +0400

    fix2

--- binary.h
+++ binary.h
@@ -9,7 +9,7 @@
 #define NC_BINARY_H_
 
 template <typename T>
-void write_buffer(char* buf, size_t len, T value, size_t& offset) {
+static void write_buffer(char* buf, size_t len, T value, size_t& offset) {
     auto size = sizeof(T);
     if (offset + size > len) {
         throw "out of range";
@@ -19,7 +19,7 @@ void write_buffer(char* buf, size_t len, T value, size_t& offset) {
 }
 
 template <typename T>
-void write_buffer(char* buf, size_t len, T value, const size_t& offset = 0) {
+static void write_buffer(char* buf, size_t len, T value, const size_t& offset = 0) {
     auto size = sizeof(T);
     if (offset + size > len) {
         throw "out of range";
@@ -28,7 +28,7 @@ void write_buffer(char* buf, size_t len, T value, const size_t& offset = 0) {
 }
 
 template <typename T>
-T read_buffer(const char* buf, size_t len, size_t& offset) {
+static T read_buffer(const char* buf, size_t len, size_t& offset) {
     auto size = sizeof(T);
     if (offset + size > len) {
         throw "out of range";
@@ -41,7 +41,7 @@ T read_buffer(const char* buf, size_t len, size_t& offset) {
 
 
 template <typename T>
-T read_buffer(const char* buf, size_t len, const size_t& offset = 0) {
+static T read_buffer(const char* buf, size_t len, const size_t& offset = 0) {
     auto size = sizeof(T);
     if (offset + size > len) {
         throw "out of range";
@@ -51,133 +51,133 @@ T read_buffer(const char* buf, size_t len, const size_t& offset = 0) {
     return res;
 }
 
-void write_int8(char* buf, size_t len, int8_t value, const size_t& offset) {
+static void write_int8(char* buf, size_t len, int8_t value, const size_t& offset) {
     return write_buffer<int8_t>(buf, len, value, offset);
 }
-void write_int8(char* buf, size_t len, int8_t value, size_t& offset) {
+static void write_int8(char* buf, size_t len, int8_t value, size_t& offset) {
     return write_buffer<int8_t>(buf, len, value, offset);
 }
-void write_uint8(char* buf, size_t len, uint8_t value, const size_t& offset) {
+static void write_uint8(char* buf, size_t len, uint8_t value, const size_t& offset) {
     return write_buffer<uint8_t>(buf, len, value, offset);
 }
-void write_uint8(char* buf, size_t len, uint8_t value, size_t& offset) {
+static void write_uint8(char* buf, size_t len, uint8_t value, size_t& offset) {
     return write_buffer<uint8_t>(buf, len, value, offset);
 }
 
-void write_int16(char* buf, size_t len, int16_t value, const size_t& offset) {
+static void write_int16(char* buf, size_t len, int16_t value, const size_t& offset) {
     return write_buffer<int16_t>(buf, len, value, offset);
 }
-void write_int16(char* buf, size_t len, int16_t value, size_t& offset) {
+static void write_int16(char* buf, size_t len, int16_t value, size_t& offset) {
     return write_buffer<int16_t>(buf, len, value, offset);
 }
-void write_uint16(char* buf, size_t len, uint16_t value, const size_t& offset) {
+static void write_uint16(char* buf, size_t len, uint16_t value, const size_t& offset) {
     return write_buffer<uint16_t>(buf, len, value, offset);
 }
-void write_uint16(char* buf, size_t len, uint16_t value, size_t& offset) {
+static void write_uint16(char* buf, size_t len, uint16_t value, size_t& offset) {
     return write_buffer<uint16_t>(buf, len, value, offset);
 }
 
-void write_int32(char* buf, size_t len, int32_t value, const size_t& offset) {
+static void write_int32(char* buf, size_t len, int32_t value, const size_t& offset) {
     return write_buffer<int32_t>(buf, len, value, offset);
 }
-void write_int32(char* buf, size_t len, int32_t value, size_t& offset) {
+static void write_int32(char* buf, size_t len, int32_t value, size_t& offset) {
     return write_buffer<int32_t>(buf, len, value, offset);
 }
-void write_uint32(char* buf, size_t len, uint32_t value, const size_t& offset) {
+static void write_uint32(char* buf, size_t len, uint32_t value, const size_t& offset) {
     return write_buffer<uint32_t>(buf, len, value, offset);
 }
-void write_uint32(char* buf, size_t len, uint32_t value, size_t& offset) {
+static void write_uint32(char* buf, size_t len, uint32_t value, size_t& offset) {
     return write_buffer<uint32_t>(buf, len, value, offset);
 }
 
-void write_int64(char* buf, size_t len, int64_t value, const size_t& offset) {
+static void write_int64(char* buf, size_t len, int64_t value, const size_t& offset) {
     return write_buffer<int64_t>(buf, len, value, offset);
 }
-void write_int64(char* buf, size_t len, int64_t value, size_t& offset) {
+static void write_int64(char* buf, size_t len, int64_t value, size_t& offset) {
     return write_buffer<int64_t>(buf, len, value, offset);
 }
-void write_uint64(char* buf, size_t len, uint64_t value, const size_t& offset) {
+static void write_uint64(char* buf, size_t len, uint64_t value, const size_t& offset) {
     return write_buffer<uint64_t>(buf, len, value, offset);
 }
-void write_uint64(char* buf, size_t len, uint64_t value, size_t& offset) {
+static void write_uint64(char* buf, size_t len, uint64_t value, size_t& offset) {
     return write_buffer<uint64_t>(buf, len, value, offset);
 }
 
-void write_float(char* buf, size_t len, float_t value, const size_t& offset) {
+static void write_float(char* buf, size_t len, float_t value, const size_t& offset) {
     return write_buffer<float_t>(buf, len, value, offset);
 }
-void write_double(char* buf, size_t len, double_t value, const size_t& offset) {
+static void write_double(char* buf, size_t len, double_t value, const size_t& offset) {
     return write_buffer<double_t>(buf, len, value, offset);
 }
-void write_float(char* buf, size_t len, float_t value, size_t& offset) {
+static void write_float(char* buf, size_t len, float_t value, size_t& offset) {
     return write_buffer<float_t>(buf, len, value, offset);
 }
-void write_double(char* buf, size_t len, double_t value, size_t& offset) {
+static void write_double(char* buf, size_t len, double_t value, size_t& offset) {
     return write_buffer<double_t>(buf, len, value, offset);
 }
 
-int8_t read_int8(const char* buf, size_t len, size_t& offset) {
+static int8_t read_int8(const char* buf, size_t len, size_t& offset) {
     return read_buffer<int8_t>(buf, len, offset);
 }
-int8_t read_int8(const char* buf, size_t len, const size_t& offset) {
+static int8_t read_int8(const char* buf, size_t len, const size_t& offset) {
     return read_buffer<int8_t>(buf, len, offset);
 }
-uint8_t read_uint8(const char* buf, size_t len, size_t& offset) {
+static uint8_t read_uint8(const char* buf, size_t len, size_t& offset) {
     return read_buffer<uint8_t>(buf, len, offset);
 }
-uint8_t read_uint8(const char* buf, size_t len, const size_t& offset) {
+static uint8_t read_uint8(const char* buf, size_t len, const size_t& offset) {
     return read_buffer<uint8_t>(buf, len, offset);
 }
 
-int16_t read_int16(const char* buf, size_t len, size_t& offset) {
+static int16_t read_int16(const char* buf, size_t len, size_t& offset) {
     return read_buffer<int16_t>(buf, len, offset);
 }
-int16_t read_int16(const char* buf, size_t len, const size_t& offset) {
+static int16_t read_int16(const char* buf, size_t len, const size_t& offset) {
     return read_buffer<int16_t>(buf, len, offset);
 }
-uint16_t read_uint16(const char* buf, size_t len, size_t& offset) {
+static uint16_t read_uint16(const char* buf, size_t len, size_t& offset) {
     return read_buffer<uint16_t>(buf, len, offset);
 }
-uint16_t read_uint16(const char* buf, size_t len, const size_t& offset) {
+static uint16_t read_uint16(const char* buf, size_t len, const size_t& offset) {
     return read_buffer<uint16_t>(buf, len, offset);
 }
 
-int32_t read_int32(const char* buf, size_t len, size_t& offset) {
+static int32_t read_int32(const char* buf, size_t len, size_t& offset) {
     return read_buffer<int32_t>(buf, len, offset);
 }
-int32_t read_int32(const char* buf, size_t len, const size_t& offset) {
+static int32_t read_int32(const char* buf, size_t len, const size_t& offset) {
     return read_buffer<int32_t>(buf, len, offset);
 }
-uint32_t read_uint32(const char* buf, size_t len, size_t& offset) {
+static uint32_t read_uint32(const char* buf, size_t len, size_t& offset) {
     return read_buffer<uint32_t>(buf, len, offset);
 }
-uint32_t read_uint32(const char* buf, size_t len, const size_t& offset) {
+static uint32_t read_uint32(const char* buf, size_t len, const size_t& offset) {
     return read_buffer<uint32_t>(buf, len, offset);
 }
 
-int64_t read_int64(const char* buf, size_t len, size_t& offset) {
+static int64_t read_int64(const char* buf, size_t len, size_t& offset) {
     return read_buffer<int64_t>(buf, len, offset);
 }
-int64_t read_int64(const char* buf, size_t len, const size_t& offset) {
+static int64_t read_int64(const char* buf, size_t len, const size_t& offset) {
     return read_buffer<int64_t>(buf, len, offset);
 }
-uint64_t read_uint64(const char* buf, size_t len, size_t& offset) {
+static uint64_t read_uint64(const char* buf, size_t len, size_t& offset) {
     return read_buffer<uint64_t>(buf, len, offset);
 }
-uint64_t read_uint64(const char* buf, size_t len, const size_t& offset) {
+static uint64_t read_uint64(const char* buf, size_t len, const size_t& offset) {
     return read_buffer<uint64_t>(buf, len, offset);
 }
 
-float_t read_float(const char* buf, size_t len, size_t& offset) {
+static float_t read_float(const char* buf, size_t len, size_t& offset) {
     return read_buffer<float_t>(buf, len, offset);
 }
-float_t read_float(const char* buf, size_t len, const size_t& offset) {
+static float_t read_float(const char* buf, size_t len, const size_t& offset) {
     return read_buffer<float_t>(buf, len, offset);
 }
-double_t read_double(const char* buf, size_t len, size_t& offset) {
+static double_t read_double(const char* buf, size_t len, size_t& offset) {
     return read_buffer<double_t>(buf, len, offset);
 }
-double_t read_double(const char* buf, size_t len, const size_t& offset) {
+static double_t read_double(const char* buf, size_t len, const size_t& offset) {
     return read_buffer<double_t>(buf, len, offset);
 }
 
